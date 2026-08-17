2c631dbaba21c0f3b43312c05ea9c5fcbdb19c84
Duy Chinh (Tony) Dinh <95660229+tonyfromvietnam@users.noreply.github.com>
2026-08-17 22:39:16 +0100

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        temp = set()
+        for num in nums:
+            if num in temp:
+                return True
+            temp.add(num)
+
+        return False
